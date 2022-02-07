bca43249f600e99a17426761454b95f571e209e0
SergeySidorchuk <serg_ss@ukr.net>
2022-02-07 17:10:10 +0200

    fixed bugs in 7 tasks

--- task7/task7.js
+++ task7/task7.js
@@ -3,12 +3,10 @@ const link =
 
 const root = document.getElementById("root");
 
-const popup = document.getElementById('popup');
-const textInput = document.getElementById('text-input')
-const form = document.getElementById('form')
-const closePopup = document.getElementById('close')
-
-  
+const popup = document.getElementById("popup");
+const textInput = document.getElementById("text-input");
+const form = document.getElementById("form");
+const closePopup = document.getElementById("close");
 
 let store = {
   city: "Kiev",
@@ -17,93 +15,89 @@ let store = {
   temperature: 0,
   observationTime: "00:00 AM",
   localTime: {},
-  isDay: "yes", 
+  isDay: "yes",
   description: 0,
   properties: {
     cloudcover: {},
     humidity: {},
     windSpeed: {},
-    visibility:{},
+    visibility: {},
     pressure: {},
     uvIndex: {},
   },
 };
-console.log(store);
+// console.log(store);
 const fetchData = async () => {
- try {
-   const query = localStorage.getItem('query') || store.city
-  const result = await fetch(`${link}&query=${query}`);
-  const data = await result.json();
+  try {
+    const query = localStorage.getItem("query") || store.city;
+    const result = await fetch(`${link}&query=${query}`);
+    const data = await result.json();
 
-  const {
-    current: {
-      cloudcover,
-      temperature,
-      humidity,
-      observation_time: observationTime,
-      pressure,
-      uv_index: uvIndex,
-      visibility,
-      is_day: isDay,
-      weather_descriptions: description,
-      wind_speed: windSpeed,
-    },
-    location: {
+    const {
+      current: {
+        cloudcover,
+        temperature,
+        humidity,
+        observation_time: observationTime,
+        pressure,
+        uv_index: uvIndex,
+        visibility,
+        is_day: isDay,
+        weather_descriptions: description,
+        wind_speed: windSpeed,
+      },
+      location: { country, name, localtime: localTime },
+    } = data;
+
+    // console.log(data);
+    store = {
+      ...store,
+      isDay,
       country,
-      name,
+      city: name,
+      temperature,
+      observationTime,
       localtime: localTime,
-    }
-  } = data;
+      description: description[0],
+      properties: {
+        cloudcover: {
+          title: "cloudcover",
+          value: `${cloudcover}%`,
+          icon: "cloud.png",
+        },
+        humidity: {
+          title: "humidity",
+          value: `${humidity}%`,
+          icon: "humidity.png",
+        },
+        windSpeed: {
+          title: "windSpeed",
+          value: `${windSpeed}m/s`,
+          icon: "wind.png",
+        },
+        pressure: {
+          title: "pressure",
+          value: `${pressure} %`,
+          icon: "gauge.png",
+        },
+        uvIndex: {
+          title: "uvIndex",
+          value: `${uvIndex} / 100`,
+          icon: "uv-index.png",
+        },
+        visibility: {
+          title: "visibility",
+          value: `${visibility}%`,
+          icon: "visibility.png",
+        },
+      },
+    };
 
-console.log(data);
-  store = {
-    ...store,
-    isDay,
-    country,
-    city: name,
-    temperature,
-    observationTime,
-    localtime:localTime,
-    description: description[0],
-    properties: {
-      cloudcover: {
-        title: 'cloudcover',
-        value: `${cloudcover}%`,
-        icon: 'cloud.png',
-      },
-      humidity:{
-        title: 'humidity',
-        value: `${humidity}%`,
-        icon: 'humidity.png',
-      },
-      windSpeed: {
-        title: 'windSpeed',
-        value: `${windSpeed}m/s`,
-        icon: 'wind.png',
-      },
-      pressure: {
-        title: 'pressure',
-        value: `${pressure} %`,
-        icon: 'gauge.png',
-      },
-      uvIndex: {
-        title: 'uvIndex',
-        value: `${uvIndex} / 100`,
-        icon: 'uv-index.png',
-      },
-      visibility: {
-        title: 'visibility',
-        value:`${visibility}%`,
-        icon: 'visibility.png',
-      },
-  },
+    renderComponent();
+  } catch (error) {
+    console.log(error.message);
+  }
 };
-
-  renderComponent();
- } catch (error) {
-   console.log(error);
- }
-}
 
 const getImage = (description) => {
   const value = description.toLowerCase();
@@ -124,8 +118,9 @@ const getImage = (description) => {
 };
 
 const renderProperty = (properties) => {
-  return Object.values(properties).map(({title, value, icon}) => {
-    return `<div class='property'>
+  return Object.values(properties)
+    .map(({ title, value, icon }) => {
+      return `<div class='property'>
     <div class='property-icon'>
       <img src='./img/icons/${icon}' alt=''>
     </div>
@@ -134,11 +129,21 @@ const renderProperty = (properties) => {
       <div class='property-info__description'>${title}</div>
     </div>
   </div>`;
-}).join('')
+    })
+    .join("");
 };
 
 const markup = () => {
-  const { country, city, description, observationTime, temperature, isDay, properties, localtime } = store;
+  const {
+    country,
+    city,
+    description,
+    observationTime,
+    temperature,
+    isDay,
+    properties,
+    localtime,
+  } = store;
   const containerClass = isDay === "yes" ? "is-day" : "";
 
   return `<div class='container ${containerClass}'>
@@ -168,42 +173,40 @@ const markup = () => {
 };
 
 const toglePoupClass = () => {
-  popup.classList.toggle('active')
-}
+  popup.classList.toggle("active");
+};
 
 const renderComponent = () => {
   root.innerHTML = markup();
 
-  const city = document.getElementById('city');
-  city.addEventListener('click', toglePoupClass);
-
+  const city = document.getElementById("city");
+  city.addEventListener("click", toglePoupClass);
 };
 
 const handleInput = (e) => {
   store = {
     ...store,
     city: e.target.value,
-  }
-}
+  };
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
-const value = store.city
+  const value = store.city;
 
-if(!value) return null;
+  if (!value) return null;
 
-localStorage.setItem('query', value)
-fetchData();
-toglePoupClass();
-}
+  localStorage.setItem("query", value);
+  fetchData();
+  toglePoupClass();
+};
 
-form.addEventListener('submit', handleSubmit)
-textInput.addEventListener('input', handleInput)
+form.addEventListener("submit", handleSubmit);
+textInput.addEventListener("input", handleInput);
 
-  const hidePopupClass = () => { 
-    popup.classList.remove('active')
-  }
-  closePopup.addEventListener('click', hidePopupClass )
-
+const hidePopupClass = () => {
+  popup.classList.remove("active");
+};
+closePopup.addEventListener("click", hidePopupClass);
 
 fetchData();
